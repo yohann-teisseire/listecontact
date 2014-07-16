bd4ff43cedcb940b8c2e5da8222aed9acce5b336
yohann-teisseire <yohan.teisseire@gmail.com>
2014-07-16 12:13:28 +0200

    Initial Commit

---
+++ ListeContact/ListeContactTests/ListeContactTests.m
@@ -0,0 +1,34 @@
+//
+//  ListeContactTests.m
+//  ListeContactTests
+//
+//  Created by yoyo2ntp on 16/07/2014.
+//  Copyright (c) 2014 account user. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ListeContactTests : XCTestCase
+
+@end
+
+@implementation ListeContactTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
